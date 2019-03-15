bc156917bfc08a4acd2638e4a7dc465918144a14
roedoejet <aidanpine@shaw.ca>
2019-03-15 16:21:01 -0700

    added styling

--- src/components/read-along-component/read-along.tsx
+++ src/components/read-along-component/read-along.tsx
@@ -1,11 +1,11 @@
 import { Component, Prop } from '@stencil/core';
 // import { Sprite } from '../../utils/sprite';
-import { Howl } from 'howler';
+import { Howl, Howler } from 'howler';
 import { parseSMIL, parseTEI } from '../../utils/utils'
 
 @Component({
   tag: 'read-along',
-  styleUrl: 'read-along.scss',
+  styleUrl: 'scss/styles.scss',
   shadow: true
 })
 export class ReadAlongComponent {
@@ -13,34 +13,31 @@ export class ReadAlongComponent {
    * The text as TEI
    */
   @Prop() text: string;
+  processed_text;
 
   /**
    * The alignment as SMIL
    */
   @Prop() alignment: string;
+  processed_alignment;
 
   /**
    * The audio file
    */
   @Prop() audio: string;
+  audio_howl: Howl;
 
   /**
    * Image
    */
   @Prop() image: string;
 
-  play(sprites?, id?) {
-    var tag = id.path[0].id
-    if (sprites) {
-      var sound = new Howl({
-        src: [this.audio],
-        sprite: sprites
-      })
-      sound.play(tag)
+  play(id?) {
+    if (id) {
+      var tag = id.path[0].id
+      this.audio_howl.play(tag)
     } else {
-      var sound = new Howl({
-        src: [this.audio]
-      })
+      this.audio_howl.play()
     }
   }
 
@@ -54,17 +51,37 @@ export class ReadAlongComponent {
     return parseSMIL(this.alignment)
   }
 
+  componentWillLoad(){
+    this.processed_alignment = this.getAlignments()
+    this.processed_text = this.getText()
+    this.audio_howl = new Howl({
+      src: [this.audio],
+      sprite: this.processed_alignment
+    })
+  }
+
   render() {
-    let text = this.getText();
-    let alignments = this.getAlignments();
     if (this.image) {
-      return <img id='waveform' src={this.image} onClick={() => this.play(alignments)}></img>
+      return <img id='waveform' src={this.image} onClick={() => this.play(false)}></img>
     } else {
       return (
         <div>
-          {text.map((seg) =>
-            <span id={seg[0]} onClick={(ev) => this.play(alignments, ev)}>{seg[1]} </span>
-          )}
+          <div class='sentence'>
+            {this.processed_text.map((seg) =>
+              <span class='sentence__word ripple' id={seg[0]} onClick={(ev) => this.play(ev)}>{seg[1]} </span>
+            )}
+          </div>
+          <div class="control-panel">
+            <button class="control-panel__control ripple">
+              <i class="material-icons" onClick={() => this.play(false)}>play_arrow</i>
+            </button>
+            <button class="control-panel__control ripple">
+              <i class="material-icons">pause</i>
+            </button>
+            <button class="control-panel__control ripple">
+              <i class="material-icons">loop</i>
+            </button>
+          </div>
         </div>
       )
     }
