d8d9c25deebeb06de7e3040b5be11cfd7954ec22
Eddie Antonio Santos <eddieantonio@hey.com>
2021-07-08 14:11:54 -0600

    test: get a test working

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,14 +1,33 @@
-context('The Readalong Component', () => {
+context.only('The Readalong Component', () => {
+  /**
+   * Wait for the audio and the SMIL to load.
+   */
+  const EXPECTED_LOADING_TIME = 2000 // ms
+
   it('should exist', () => {
     cy.visit('/ej-fra/')
 
-    cy.get('read-along')
-      .first()
-      .should('exist')
-      .as('read-along')
+    theReadalong()
+      .should('be.visible')
 
-    cy.get('@read-along')
-      .should('contain', 'Bonjour')
-      .click()
+    cy.wait(EXPECTED_LOADING_TIME)
+
+    withinTheReadalong()
+      .contains('Bonjour')
+      .click();
   })
+
+  function theReadalong() {
+    return cy.get('read-along').first();
+  }
+
+  function withinTheReadalong() {
+    /**
+     * To do any asserts of content WITHIN the <read-along> component, you
+     * need to use .shadow() to dive into the shadow DOM.
+     *
+     * https://docs.cypress.io/api/commands/shadow
+     */
+    return theReadalong().shadow()
+  }
 })
