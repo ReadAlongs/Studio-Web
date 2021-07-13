879805700e37cc147f47d2f10c9e2055f4d02ac7
Eddie Antonio Santos <Eddie.Santos@nrc-cnrc.gc.ca>
2021-07-13 11:26:22 -0600

    [wip] configure stencil dev server?

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,33 @@
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
-  namespace: 'read-along',
-  outputTargets:[
-    { type: 'dist' },
-    { type: 'docs-readme' },
+  namespace: "read-along",
+  outputTargets: [
+    { type: "dist" },
+    { type: "docs-readme" },
     {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
+      type: "www",
+      serviceWorker: null, // disable service workers
+    },
   ],
-  plugins: [
-    sass()
-  ]
+  plugins: [sass()],
+  devServer: {
+    ...devServerOverrides(),
+  },
 };
+
+function devServerOverrides() {
+  if (Boolean(process.env.RUNNING_INTEGRATION_TESTS)) {
+    return {
+      // Cypress will open its own browser
+      openBrowser: false,
+      // Have the tests explicitly reload functionality rather than using
+      // hot-module replacement.
+      reloadStrategy: null,
+    };
+  }
+
+  // Not running tests -- do not override anything:
+  return {};
+}
