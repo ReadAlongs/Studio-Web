315fddc1a807edfb41b6b83ec9bba84f3334a2b9
roedoejet <aidanpine@shaw.ca>
2022-06-30 16:10:01 -0700

    feat(api): add language names from api

--- src/app/g2p.service.ts
+++ src/app/g2p.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class G2pService {
-  langsURL = "//g2p-studio.herokuapp.com/api/v1/langs"
+  langsURL = "//g2p-studio.herokuapp.com/api/v1/langs?verbose=true"
   constructor(private http: HttpClient) {
   }
 
   getLangs$(): Observable<any> {
-    return this.http.get(this.langsURL)
+    return this.http.get(this.langsURL).pipe(
+      map((langs: any) => langs.filter((x: any) => { return x.out_lang.endsWith('ipa') && x.out_lang !== 'eng-ipa' }))
+    )
   }
 }
