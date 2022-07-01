a95c96605d21c64163f97377e5002cf69e05bb01
roedoejet <aidanpine@shaw.ca>
2022-07-01 11:49:52 -0700

    feat(ssjs): added and initialize soundswallower

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { UploadComponent } from './upload/upload.component';
 import { MatStepper } from '@angular/material/stepper';
+import * as soundswallower from 'soundswallower';
+import { BehaviorSubject, from } from 'rxjs';
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -11,14 +14,22 @@ import { MatStepper } from '@angular/material/stepper';
 export class AppComponent {
   firstFormGroup: any;
   title = 'readalong-studio';
+  alignerReady$ = new BehaviorSubject<boolean>(false);
   @ViewChild('upload', { static: false }) upload?: UploadComponent;
   @ViewChild("stepper") private stepper: MatStepper;
   constructor() {
 
   }
   ngOnInit(): void {
-
+    let ssjs$ = from(this.loadSS()).pipe(take(1))
+    ssjs$.subscribe((_) => this.alignerReady$.next(true))
   }
+
+  async loadSS() {
+    const ssjs = await soundswallower.initialize('assets/model/en-us')
+    return ssjs
+  }
+
   formChanged(formGroup: FormGroup) {
     this.firstFormGroup = formGroup
   }
