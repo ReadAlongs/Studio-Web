e9c9cfb197d4635e2db64b4499d7806f6132a2a1
yox chen <yoxchen@yoxdeMBP.lan>
2022-10-31 11:59:33 -0700

    test: unit testing for feature editable page & subtitle

--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -6,7 +6,7 @@ import { By } from "@angular/platform-browser";
 import { MaterialModule } from "../material.module";
 import { DemoComponent } from "./demo.component";
 
-// ==== check create or not and defalut value =====
+// ==== check create or not and default value =====
 describe("DemoComponent", () => {
   let component: DemoComponent;
   let fixture: ComponentFixture<DemoComponent>;
@@ -20,23 +20,61 @@ describe("DemoComponent", () => {
 
     fixture = TestBed.createComponent(DemoComponent);
     component = fixture.componentInstance;
+    component.b64Inputs = ["true"];
+    component.slots.title = "new title";
     fixture.detectChanges();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  it("test edit title", () => {
+    const { debugElement } = fixture;
+    const title1 = fixture.debugElement.query(By.css("#divid1"));
+    title1.nativeElement.value = "new title";
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBe("new title");
   });
 
-  it(`should have as title 'Title'`, () => {
-    expect(component.slots.title).toEqual("Title");
+  it("test edit title", () => {
+    const { debugElement } = fixture;
+    const title1 = fixture.debugElement.query(By.css("#divid2"));
+    title1.nativeElement.value = "new sub title";
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBe("new sub title");
   });
 
-  it(`should have as subtitle 'SubTitle'`, () => {
-    expect(component.slots.subtitle).toEqual("Subtitle");
+  it("test edit title", () => {
+    const { debugElement } = fixture;
+    const title1 = fixture.debugElement.query(By.css("#divid1"));
+    title1.nativeElement.value = "";
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBeNull;
   });
 
-  it(`should have as page title 'ReadAlong Studio'`, () => {
-    expect(component.slots.pageTitle).toEqual("ReadAlong Studio");
+  it("test edit title", () => {
+    const { debugElement } = fixture;
+    const title1 = fixture.debugElement.query(By.css("#divid2"));
+    title1.nativeElement.value = "";
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBeNull;
+  });
+
+  it("test edit title more than once", () => {
+    const title1 = fixture.debugElement.query(By.css("#divid1"));
+    for (let i = 1; i <= 50; i++) {
+      title1.nativeElement.value = "title " + i;
+    }
+
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBe("title 50");
+  });
+
+  it("test edit sub title", () => {
+    const title1 = fixture.debugElement.query(By.css("#divid2"));
+    for (let i = 1; i <= 50; i++) {
+      title1.nativeElement.value = "sub title " + i;
+    }
+
+    title1.nativeElement.dispatchEvent(new Event("input"));
+    expect(title1.nativeElement.value).toBe("sub title 50");
   });
 
   it("test editable page title when it is simple value", () => {
