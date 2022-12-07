ba7bb1166c36e7b559185461a5d02ca17de9f9bb
David Huggins-Daines <dhdaines@gmail.com>
2022-12-06 22:13:01 -0500

    fix: use standardized-audio-context

--- src/app/audio.service.ts
+++ src/app/audio.service.ts
@@ -1,6 +1,7 @@
 import { from, sample } from "rxjs";
 
 import { Injectable } from "@angular/core";
+import { AudioContext } from "standardized-audio-context";
 
 @Injectable({
   providedIn: "root",
@@ -9,7 +10,6 @@ export class AudioService {
   constructor() {}
 
   loadAudioBufferFromFile$(file: File) {
-    const AudioContext = window.AudioContext;
     var audioCtx = new AudioContext({ sampleRate: 16000 });
     var audioFile = file.arrayBuffer().then((buffer: any) => {
       return audioCtx.decodeAudioData(buffer);
