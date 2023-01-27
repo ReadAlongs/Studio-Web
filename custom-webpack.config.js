4e5ac6625684e24c716c73dd97049340d228dc14
David Huggins-Daines <dhdaines@gmail.com>
2023-01-27 00:32:04 -0500

    fix: make wasm get bundled by webpack as the good lord intended

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  // Stop webpack from munging import.meta.url (this should be the default in
-  // Angular but unfortunately it isn't)
   module: {
     parser: {
       javascript: {
-        importMeta: false,
+        url: true,
       },
     },
   },
