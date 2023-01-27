ad2d42650f71017bd1d31a108919dac902c1ed0f
roedoejet <hello@aidanpine.ca>
2023-01-27 15:38:59 -0800

    refactor: unify angular dependencies across packages

--- packages/angular-demo/src/app/app.module.ts
+++ packages/angular-demo/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { NgxRAWebComponentModule } from '@readalongs/ngx-web-component';
+import { defineCustomElements } from '@readalongs/web-component/loader';
+
+defineCustomElements()
 
 @NgModule({
   declarations: [
