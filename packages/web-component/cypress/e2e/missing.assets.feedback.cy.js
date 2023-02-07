3d511633979fff21f363dee4582318b0b731263c
David Huggins-Daines <dhdaines@gmail.com>
2023-02-06 23:21:32 -0500

    test: update for new reality

--- packages/web-component/cypress/e2e/missing.assets.feedback.cy.js
+++ packages/web-component/cypress/e2e/missing.assets.feedback.cy.js
@@ -21,8 +21,7 @@ context("Readalong Component with missing assets", () => {
         //check that message is visible
         expect($el.text()).contains("The text file could not be loaded", "error message visible")
       }).should("be.visible")
-      cy.get("[data-cy=alignment-error]").should('have.class', 'fade').should("not.be.visible")
-      cy.get("[data-cy=progress-bar]").should("have.length", 1).should("be.visible")
+      cy.get("[data-cy=progress-bar]").should("have.length", 0)
 
     });
   });
@@ -42,7 +41,6 @@ context("Readalong Component with missing assets", () => {
       }).should("be.visible")
       cy.get("[data-cy=control-panel]").should("have.length", 0)
       cy.get("[data-cy=text-error]").should('have.class', 'fade').should("not.be.visible")
-      cy.get("[data-cy=alignment-error]").should('have.class', 'fade').should("not.be.visible")
       cy.get("[data-cy=progress-bar]").should("have.length", 1)
 
     });
@@ -61,7 +59,7 @@ context("Readalong Component with missing assets", () => {
       cy.get("[data-cy=alignment-error]").should(($el) => {
         expect($el.hasClass("fade")).equal(false)
         // check that message is visible
-        expect($el.text()).contains("Error: The alignment file could not be loaded")
+        expect($el.text()).contains("Error: No alignments were found")
       }).should("be.visible")
       cy.get("[data-cy=progress-bar]").should("have.length", 0)
 
