cea27cce02223657e0f2fc4d2ef42c5b39604bf2
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-02-14 13:00:11 -0500

    test: update unit testing for improved error message handling

--- packages/web-component/cypress/e2e/missing.assets.feedback.cy.js
+++ packages/web-component/cypress/e2e/missing.assets.feedback.cy.js
@@ -1,46 +1,67 @@
 context("Readalong Component with missing assets", () => {
   /**
-   * Wait for the audio and the SMIL to load.
+   * Wait for the audio and the RAS to load.
    */
-  const EXPECTED_LOADING_TIME = 3000; // ms
-
+  const EXPECTED_LOADING_TIME = 2000; // ms
 
   it("missing text warning show show successfully", () => {
     cy.visit("/ej-fra/index-missing-xml.html");
-    cy.wait(EXPECTED_LOADING_TIME);
+    //cy.wait(EXPECTED_LOADING_TIME);
     cy.readalongElement().should("be.visible");
 
     cy.readalong().within(() => {
       cy.get("[data-cy=text-container]").should(($el) => {
         expect($el.children().length).equal(0, "has text")
       })
-      cy.get("[data-cy=audio-error]").should('have.class', 'fade').should("not.be.visible")
+      cy.get("[data-cy=AUDIO-error]").should("not.exist")
+      cy.get("[data-cy=alignment-error]").should("not.exist")
       cy.get("[data-cy=control-panel]").should("have.length", 1).should("be.visible")
-      cy.get("[data-cy=text-error]").should(($el) => {
+      cy.get("[data-cy=RAS-error]").should(($el) => {
         expect($el.hasClass("fade")).equal(false, "error message box visible")
         //check that message is visible
-        expect($el.text()).contains("The text file could not be loaded", "error message visible")
+        expect($el.text()).contains("the RAS file 'ejfra.ras' could not be loaded", "error message visible")
       }).should("be.visible")
       cy.get("[data-cy=progress-bar]").should("have.length", 0)
 
     });
   });
 
+  it("parsing text warning should show successfully", () => {
+    cy.visit("/ej-fra/index-malformed-xml.html");
+    //cy.wait(EXPECTED_LOADING_TIME);
+    cy.readalongElement().should("be.visible");
+
+    cy.readalong().within(() => {
+      cy.get("[data-cy=text-container]").should(($el) => {
+        expect($el.children().length).equal(0, "has text")
+      })
+      cy.get("[data-cy=AUDIO-error]").should("not.exist")
+      cy.get("[data-cy=alignment-error]").should("not.exist")
+      cy.get("[data-cy=control-panel]").should("have.length", 1).should("be.visible")
+      cy.get("[data-cy=RAS-error]").should(($el) => {
+        expect($el.hasClass("fade")).equal(false, "error message box visible")
+        //check that message is visible
+        expect($el.text()).contains("Erreur: le fichier RAS 'ej-fra-malformed.ras' n'a pas pu être analysé", "error message visible")
+      }).should("be.visible")
+      cy.get("[data-cy=progress-bar]").should("have.length", 0)
+    });
+  });
+
   it("missing audio warning should show successfully", () => {
     cy.visit("/ej-fra/index-missing-audio.html");
-    cy.wait(EXPECTED_LOADING_TIME);
+    //cy.wait(EXPECTED_LOADING_TIME);
     cy.readalongElement().should("be.visible");
 
     cy.readalong().within(() => {
       cy.contains("Page");
-      return; // TODO fix this test case and then reactivate it
-      cy.get("[data-cy=audio-error]").should(($el) => {
+      cy.get("[data-cy=AUDIO-error]").should(($el) => {
         expect($el.hasClass("fade")).equal(false)
         //check that message is visible
-        expect($el.text()).contains("Error: The audio file could not be loaded")
+        expect($el.text()).contains("Error: the AUDIO file 'ejfra.m4a' could not be loaded")
       }).should("be.visible")
       cy.get("[data-cy=control-panel]").should("have.length", 0)
-      cy.get("[data-cy=text-error]").should('have.class', 'fade').should("not.be.visible")
+      cy.get("[data-cy=RAS-error]").should("not.exist")
+      cy.get("[data-cy=alignment-error]").should("not.exist")
       cy.get("[data-cy=progress-bar]").should("have.length", 1)
 
     });
@@ -53,9 +74,9 @@ context("Readalong Component with missing assets", () => {
 
     cy.readalong().within(() => {
       cy.contains("Page");
-      cy.get("[data-cy=audio-error]").should('have.class', 'fade').should("not.be.visible")
+      cy.get("[data-cy=AUDIO-error]").should("not.exist")
       cy.get("[data-cy=control-panel]").should("have.length", 1).should("be.visible")
-      cy.get("[data-cy=text-error]").should('have.class', 'fade').should("not.be.visible")
+      cy.get("[data-cy=RAS-error]").should("not.exist")
       cy.get("[data-cy=alignment-error]").should(($el) => {
         expect($el.hasClass("fade")).equal(false)
         // check that message is visible
@@ -63,6 +84,18 @@ context("Readalong Component with missing assets", () => {
       }).should("be.visible")
       cy.get("[data-cy=progress-bar]").should("have.length", 0)
 
+    });
+  });
+
+  it("should only show missing audio and ras when everything is missing", () => {
+    cy.visit("/ej-fra/index-missing-everything.html");
+    cy.wait(EXPECTED_LOADING_TIME);
+    cy.readalongElement().should("be.visible");
+
+    cy.readalong().within(() => {
+      cy.get("[data-cy=alignment-error]").should("not.exist");
+      cy.get("[data-cy=RAS-error]").should("be.visible");
+      cy.get("[data-cy=AUDIO-error]").should("be.visible");
     });
   });
 
