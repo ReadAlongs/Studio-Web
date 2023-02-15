3b4e4986b826813658751285fc9cd682fecce781
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-02-15 13:16:03 -0500

    fix(test): dang it, we do need that 2s wait to load

--- packages/web-component/cypress/e2e/basic.cy.js
+++ packages/web-component/cypress/e2e/basic.cy.js
@@ -1,4 +1,5 @@
 context("The Readalong Component", () => {
+  const EXPECTED_LOADING_TIME = 2000; // ms
   const FOR_PAGE_TURN_ANIMATION = 500; // ms
   const FOR_ERIC_TO_TALK_A_BIT = 3000; // ms
 
@@ -15,6 +16,8 @@ context("The Readalong Component", () => {
   });
 
   it("should play the entire ReadAlong", () => {
+    cy.wait(EXPECTED_LOADING_TIME);
+
     cy.readalong().within(() => {
       cy.get("[data-cy=play-button]").click();
       cy.wait(FOR_ERIC_TO_TALK_A_BIT);
@@ -23,11 +26,15 @@ context("The Readalong Component", () => {
   });
 
   it("should play a single word when clicked", () => {
+    cy.wait(EXPECTED_LOADING_TIME);
+
     cy.readalong().contains("technologies").click();
   });
 
   describe("the progress bar", () => {
     it("should skip ahead when clicked", () => {
+      cy.wait(EXPECTED_LOADING_TIME);
+
       cy.readalong().within(() => {
         cy.get("[data-cy=play-button]").click();
         cy.get("[data-cy=page-count__current]")
