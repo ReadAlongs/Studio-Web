e3ff8517a9403757c83a0cd05fd9271b6e4bb7e4
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-02-17 17:43:45 -0500

    feat: add caveat about not using the browser back button

--- packages/studio-web/src/app/shepherd.steps.ts
+++ packages/studio-web/src/app/shepherd.steps.ts
@@ -1,5 +1,5 @@
 export const intro_step: any = {
-  title: $localize`Welcome to ReadAlong Studio\n`,
+  title: $localize`Welcome to ReadAlong Studio`,
   text: $localize`Creating a ReadAlong is easy!\
     This guide will show you all the bells and whistles of the Studio.`,
   attachTo: {
@@ -17,7 +17,7 @@ export const intro_step: any = {
 };
 
 export const data_step: any = {
-  title: $localize`Adding your data\n`,
+  title: $localize`Adding your data`,
   text: $localize`To make your ReadAlong, you'll need to add your text and audio.`,
   attachTo: {
     element: "#upload-header",
@@ -39,7 +39,7 @@ export const data_step: any = {
 };
 
 export const text_write_step: any = {
-  title: $localize`Write your text\n`,
+  title: $localize`Write your text`,
   text: $localize`You can write your text directly into ReadAlong Studio, by selecting the "write" option.`,
   attachTo: {
     element: "#text-section",
@@ -61,7 +61,7 @@ export const text_write_step: any = {
 };
 
 export const text_file_step: any = {
-  title: $localize`Use a text file\n`,
+  title: $localize`Use a text file`,
   text: $localize`You can also use text from a plain text file (.txt) or a file in the RAS format (.readalong).`,
   attachTo: {
     element: "#text-section",
@@ -83,7 +83,7 @@ export const text_file_step: any = {
 };
 
 export const audio_record_step: any = {
-  title: $localize`Record your own audio\n`,
+  title: $localize`Record your own audio`,
   text: $localize`You can record your own audio for preprocessing using your browser's microphone.`,
   attachTo: {
     element: "#audio-section",
@@ -105,7 +105,7 @@ export const audio_record_step: any = {
 };
 
 export const audio_file_step: any = {
-  title: $localize`Use an audio file\n`,
+  title: $localize`Use an audio file`,
   text: $localize`You can also select either a .wav or .mp3 file for your ReadAlong.`,
   attachTo: {
     element: "#audio-section",
@@ -127,7 +127,7 @@ export const audio_file_step: any = {
 };
 
 export const language_step: any = {
-  title: $localize`Select your language\n`,
+  title: $localize`Select your language`,
   text: $localize`Then, select the language of your ReadAlong. We support over 30 different languages, but if your language is not here, you can try using the Undetermined (und) option. If that doesn't work very well, you can always add your own language. This requires some understanding of your language's writing system. Feel free to reach out to us or visit our <a href="https://blog.mothertongues.org/g2p-background/" target="_blank" rel="noopener noreferrer">blog posts</a> for more information.`,
   attachTo: {
     element: "#language-section",
@@ -149,7 +149,7 @@ export const language_step: any = {
 };
 
 export const step_one_final_step: any = {
-  title: $localize`That's it!\n`,
+  title: $localize`That's it!`,
   text: $localize`Once you've done this, you can click the "next step" button here to let Studio build your ReadAlong! (This may take a few seconds.)`,
   attachTo: {
     element: "#next-step",
@@ -170,7 +170,7 @@ export const step_one_final_step: any = {
 };
 
 export const step_two_intro_step: any = {
-  title: $localize`Tadaa!\n`,
+  title: $localize`Tadaa!`,
   text: $localize`Now your ReadAlong has been created.`,
   attachTo: {
     element: "#readalong",
@@ -187,7 +187,7 @@ export const step_two_intro_step: any = {
 };
 
 export const readalong_play_step: any = {
-  title: $localize`Play your ReadAlong\n`,
+  title: $localize`Play your ReadAlong`,
   text: $localize`Pressing play will play the audio and highlight the text at the same time. Try it out!`,
   buttons: [
     {
@@ -205,7 +205,7 @@ export const readalong_play_step: any = {
 };
 
 export const readalong_play_word_step: any = {
-  title: $localize`Play your ReadAlong\n`,
+  title: $localize`Play your ReadAlong`,
   text: $localize`Clicking on an individual word will play it as well.`,
   buttons: [
     {
@@ -223,7 +223,7 @@ export const readalong_play_word_step: any = {
 };
 
 export const readalong_change_title_step: any = {
-  title: $localize`Edit your ReadAlong\n`,
+  title: $localize`Edit your ReadAlong`,
   text: $localize`You can edit the text here to add a Title or Subtitle to your ReadAlong.`,
   attachTo: {
     element: "input",
@@ -245,7 +245,7 @@ export const readalong_change_title_step: any = {
 };
 
 export const readalong_add_image_step: any = {
-  title: $localize`Edit your ReadAlong\n`,
+  title: $localize`Edit your ReadAlong`,
   text: $localize`You can add an image to each page of your ReadAlong here.`,
   buttons: [
     {
@@ -263,7 +263,7 @@ export const readalong_add_image_step: any = {
 };
 
 export const readalong_add_translation_step: any = {
-  title: $localize`Edit your ReadAlong\n`,
+  title: $localize`Edit your ReadAlong`,
   text: $localize`You can add a translation or extra bit of information to each sentence of your ReadAlong by clicking on the + icon here. If you later want to remove the translation, click on the − icon after adding your translation.`,
   buttons: [
     {
@@ -281,7 +281,7 @@ export const readalong_add_translation_step: any = {
 };
 
 export const readalong_export_step: any = {
-  title: $localize`Export your ReadAlong\n`,
+  title: $localize`Export your ReadAlong`,
   text: $localize`When you're happy with your ReadAlong, you can export it by selecting a download format and then clicking on the download button (down arrow). You can choose from the following formats: `,
   attachTo: {
     element: "div.download__buttons",
@@ -303,11 +303,19 @@ export const readalong_export_step: any = {
 };
 
 export const readalong_go_back_step: any = {
-  title: $localize`Edit your ReadAlong\n`,
-  text: $localize`If you'd like to change your audio or text you can always go back to Step 1 and change things or make a new ReadAlong, but be careful! Going back to the previous step will mean you will lose all of your translations and images.`,
+  title: $localize`Edit your ReadAlong inputs`,
+  text: (
+    $localize`If you'd like to change your audio or text you can always go back to Step 1 and change things or make a new ReadAlong. ` +
+    $localize`But be careful, going back to the previous step will mean you will lose all of your translations and images.` +
+    "<br/><br/>" +
+    $localize`Warning: Using the browser's back button will leave the app and might also erase all your text and audio data, so it should be avoided. ` +
+    $localize`Instead, use the "Step 1" button to go back and change your text or audio.`
+  ),
   attachTo: {
+    // TODO: find an anchor that doesn't depend on Angular innards
+    // We want this as undet the "Step 1" icon but we can't add any id to that since it's generated
     element: "div.mat-horizontal-stepper-header-container",
-    on: "bottom"
+    on: "botton-start"
   },
   buttons: [
     {
