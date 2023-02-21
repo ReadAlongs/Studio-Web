25b1f990c217e27536596b9d9ebfcc954737786e
David Huggins-Daines <dhd@ecolingui.ca>
2023-02-21 08:52:16 -0500

    feat: use ESM in TypeScript, needed for next SoundSwallower

--- packages/studio-web/src/app/shepherd.service.ts
+++ packages/studio-web/src/app/shepherd.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@angular/core";
 import Shepherd from "shepherd.js";
+// Shepherd types have useless namespaces that break with "nodenext"
+// resolution so we can't use them
+const ShepherdIsBroken: any = Shepherd;
 import { elementIsHidden } from "./utils/dom";
 import { makeButton } from "./utils/buttons";
-import Step from "shepherd.js/src/types/step";
 @Injectable({
   providedIn: "root",
 })
 export class ShepherdService {
   confirmCancel = false;
   confirmCancelMessage?: string;
-  defaultStepOptions: Step.StepOptions = {};
+  defaultStepOptions: any = {}; // Should be Shepherd.Step.StepOptions?
   errorTitle = null;
   isActive = false;
   keyboardNavigation = true;
@@ -17,7 +19,7 @@ export class ShepherdService {
   modal = false;
   requiredElements = [];
   tourName = undefined;
-  tourObject: Shepherd.Tour;
+  tourObject: any; // Should be Shepherd.Tour?
 
   constructor() {}
 
@@ -84,7 +86,8 @@ export class ShepherdService {
    * Take a set of steps and create a tour object based on the current configuration
    * @param steps An array of steps
    */
-  addSteps(steps: Array<Step.StepOptions>) {
+  addSteps(steps: Array<any>) {
+    // Should be Shepherd.StepOptions?
     this._initialize();
     const tour = this.tourObject;
 
@@ -146,7 +149,7 @@ export class ShepherdService {
    * Initializes the tour, creates a new Shepherd.Tour. sets options, and binds events
    */
   private _initialize() {
-    const tourObject = new Shepherd.Tour({
+    const tourObject = new ShepherdIsBroken.Tour({
       confirmCancel: this.confirmCancel,
       confirmCancelMessage: this.confirmCancelMessage,
       defaultStepOptions: this.defaultStepOptions,
