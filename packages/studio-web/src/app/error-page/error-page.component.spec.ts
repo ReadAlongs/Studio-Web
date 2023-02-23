ee953acc5ed18ca5cbad264aa6ee6f0833c13971
roedoejet <hello@aidanpine.ca>
2023-02-23 14:07:14 -0800

    fix(tests): add routing module to test

--- packages/studio-web/src/app/error-page/error-page.component.spec.ts
+++ packages/studio-web/src/app/error-page/error-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { ErrorPageComponent } from "./error-page.component";
 
 describe("ErrorPageComponent", () => {
@@ -9,6 +9,7 @@ describe("ErrorPageComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ErrorPageComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorPageComponent);
