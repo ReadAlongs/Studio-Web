59ce352e19de460ccb6b8cc33f3e5c9be9e63906
roedoejet <hello@aidanpine.ca>
2023-02-23 14:02:21 -0800

    fix: add specific aligner error message

--- packages/studio-web/src/app/error-page/error-page.component.ts
+++ packages/studio-web/src/app/error-page/error-page.component.ts
@@ -9,10 +9,12 @@ import { map, Observable } from "rxjs";
 })
 export class ErrorPageComponent implements OnInit {
   msg: string | null = null;
+  errorType: string | null = null;
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.msg = this.route.snapshot.queryParamMap.get("msg");
+    this.errorType = this.route.snapshot.queryParamMap.get("errorType");
   }
 
   goHome() {
