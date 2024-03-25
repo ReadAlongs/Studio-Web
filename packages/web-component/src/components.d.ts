257c802d846c5f5fe20b4470586aa00197854eae
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-25 17:57:44 -0400

    chore(deps): migrate all the things to nx 18 and angular 17

--- packages/web-component/src/components.d.ts
+++ packages/web-component/src/components.d.ts
@@ -6,6 +6,7 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 import { InterfaceLanguage, ReadAlongMode } from "./index.d";
+export { InterfaceLanguage, ReadAlongMode } from "./index.d";
 export namespace Components {
     interface ReadAlong {
         /**
