f247699fa6fbb08927512eeabf1379adb8186705
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-25 17:57:44 -0400

    chore(deps): migrate all the things to nx 18 and angular 17

--- packages/angular-demo/src/app/app.module.ts
+++ packages/angular-demo/src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { NxWelcomeComponent } from "./nx-welcome.component";
 import { NgxRAWebComponentModule } from "@readalongs/ngx-web-component";
 import { defineCustomElements } from "@readalongs/web-component/loader";
 
 defineCustomElements();
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, NgxRAWebComponentModule],
   providers: [],
   bootstrap: [AppComponent],
