576953c8f74a4c3e87052ee2913b3c3b19307361
Aidan Pine <hello@aidanpine.ca>
2024-04-02 15:04:07 -0700

    feat: use unidecode to create filenames

--- packages/studio-web/src/app/utils/utils.ts
+++ packages/studio-web/src/app/utils/utils.ts
@@ -1,12 +1,12 @@
+import unidecode from "unidecode";
+
 export const slugify = (str: string, character_limit: number = 0) => {
   // Adapted from https://byby.dev/js-slugify-string
   // Character limit of 0 (default) means there is no limit
-  const slug = String(str)
+  const slug = String(unidecode(str))
     .normalize("NFC") // split accented characters into their base characters and diacritical marks
-    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
     .trim() // trim leading or trailing whitespace
     .toLowerCase() // convert to lowercase
-    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
     .replace(/\s+/g, "-") // replace spaces with hyphens
     .replace(/-+/g, "-"); // remove consecutive hyphens
 
