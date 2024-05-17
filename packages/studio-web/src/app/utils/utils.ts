e743018b3b1857a3b3726dc9255cec65664efcef
Aidan Pine <hello@aidanpine.ca>
2024-05-17 10:59:10 -0700

    fix: replace slugify function with library

--- packages/studio-web/src/app/utils/utils.ts
+++ packages/studio-web/src/app/utils/utils.ts
@@ -1,14 +1,17 @@
 import unidecode from "unidecode";
+import librarySlugify from "@sindresorhus/slugify";
 
 export const slugify = (str: string, character_limit: number = 0) => {
   // Adapted from https://byby.dev/js-slugify-string
   // Character limit of 0 (default) means there is no limit
-  const slug = String(unidecode(str))
-    .normalize("NFC") // split accented characters into their base characters and diacritical marks
-    .trim() // trim leading or trailing whitespace
-    .toLowerCase() // convert to lowercase
-    .replace(/\s+/g, "-") // replace spaces with hyphens
-    .replace(/-+/g, "-"); // remove consecutive hyphens
+  const slug = librarySlugify(
+    String(unidecode(str))
+      .normalize("NFC") // split accented characters into their base characters and diacritical marks
+      .trim() // trim leading or trailing whitespace
+      .toLowerCase() // convert to lowercase
+      .replace(/\s+/g, "-") // replace spaces with hyphens
+      .replace(/-+/g, "-"), // remove consecutive hyphens
+  );
 
   if (character_limit) {
     return slug.substring(0, character_limit);
