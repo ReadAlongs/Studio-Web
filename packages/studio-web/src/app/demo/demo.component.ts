35ae243873820f29a6c30f2efb4c5ae0f164c455
Aidan Pine <hello@aidanpine.ca>
2024-07-22 17:35:13 -0700

    refactor: move shared download button logic into parent components
    replace download button in component with event emitter

--- packages/studio-web/src/app/demo/demo.component.ts
+++ packages/studio-web/src/app/demo/demo.component.ts
@@ -1,11 +1,19 @@
 import { Subject } from "rxjs";
 
-import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  OnDestroy,
+  AfterViewInit,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { Components } from "@readalongs/web-component/loader";
 
 import { B64Service } from "../b64.service";
 import { StudioService } from "../studio/studio.service";
 import { DownloadService } from "../shared/download/download.service";
+import { SupportedOutputs } from "../ras.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: "app-demo",
@@ -16,11 +24,11 @@ export class DemoComponent implements OnDestroy, OnInit {
   @ViewChild("readalong") readalong!: Components.ReadAlong;
   language: "eng" | "fra" | "spa" = "eng";
   unsubscribe$ = new Subject<void>();
-
   constructor(
     public b64Service: B64Service,
     public studioService: StudioService,
     private downloadService: DownloadService,
+    private toastr: ToastrService,
   ) {
     // If we do more languages, this should be a lookup table
     if ($localize.locale == "fr") {
@@ -32,19 +40,41 @@ export class DemoComponent implements OnDestroy, OnInit {
 
   ngOnInit(): void {}
 
+  ngAfterViewInit(): void {}
+  download(download_type: SupportedOutputs) {
+    if (
+      this.studioService.b64Inputs$.value &&
+      this.studioService.b64Inputs$.value[1]
+    ) {
+      this.downloadService.download(
+        download_type,
+        this.studioService.b64Inputs$.value[0],
+        this.studioService.b64Inputs$.value[1],
+        this.studioService.slots,
+        this.readalong,
+      );
+    } else {
+      this.toastr.error($localize`Download failed.`, $localize`Sorry!`, {
+        timeOut: 10000,
+      });
+    }
+  }
+
   async ngOnDestroy() {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
     // Save translations, images and all other edits to the studio service when we exit
-    await this.downloadService.updateTranslations(
-      this.studioService.b64Inputs$.value[1],
-      this.readalong,
-    );
-    await this.downloadService.updateImages(
-      this.studioService.b64Inputs$.value[1],
-      true,
-      "image",
-      this.readalong,
-    );
+    if (this.studioService.b64Inputs$.value[1]) {
+      await this.downloadService.updateTranslations(
+        this.studioService.b64Inputs$.value[1],
+        this.readalong,
+      );
+      await this.downloadService.updateImages(
+        this.studioService.b64Inputs$.value[1],
+        true,
+        "image",
+        this.readalong,
+      );
+    }
   }
 }
