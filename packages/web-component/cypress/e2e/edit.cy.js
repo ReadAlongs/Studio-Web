5717891e87c84b92b7d5257aa0c9e5674ce5e04b
Delasie Torkornoo <Delasie.Torkornoo@nrc-cnrc.gc.ca>
2024-07-29 17:01:36 -0400

    test: fix test for editing readalong

--- packages/web-component/cypress/e2e/edit.cy.js
+++ packages/web-component/cypress/e2e/edit.cy.js
@@ -5,30 +5,39 @@ context("The Readalong Component", () => {
   const EXPECTED_LOADING_TIME = 2000; // ms
 
   beforeEach(() => {
+    cy.intercept(/\.readalong/).as("text");
+    cy.intercept(/\.m4a/).as("audio");
     cy.visit("/ej-fra/index-edit.html");
   });
 
   it("should have editable translation buttons", () => {
-    cy.wait(EXPECTED_LOADING_TIME);
+    cy.wait(["@text", "@audio"]);
+
     cy.readalongElement().should("be.visible");
 
     cy.readalong().within(() => {
+      cy.get("[data-test-id=annotation-layer]").should("have.length", 0);
       // Click first line add translation
-      cy.get("[data-cy=add-translation-button]").first().click();
+      cy.get("[data-test-id=add-translation-button]").first().click();
       // Check translation line was added
-      cy.get("[data-cy=translation-line]").should("have.length", 1);
+      cy.get("[data-test-id=annotation-layer]").should("have.length", 1);
       // Check you can type into the new translation line
-      cy.get("[data-cy=translation-line]").type("this is a test", {
-        force: true,
-      });
+      cy.get("[data-test-id=annotation-layer] span.sentence__text").type(
+        "this is a test",
+        {
+          force: true,
+        },
+      );
       // Create a new translation line with the last sentence
-      cy.get("[data-cy=add-translation-button]").last().click();
+      cy.get("[data-test-id=add-translation-button]").last().click();
       // Check it was added
-      cy.get("[data-cy=translation-line]").should("have.length", 2);
+      cy.get("[data-test-id=annotation-layer]").should("have.length", 2);
       // Remove the first line
-      cy.get("[data-cy=remove-translation-button]").first().click();
+      cy.get("[data-test-id^=remove-annotation-]")
+        .first()
+        .click({ force: true });
       // Check it was removed
-      cy.get("[data-cy=translation-line]").should("have.length", 1);
+      cy.get("[data-test-id=annotation-layer]").should("have.length", 1);
     });
   });
 });
