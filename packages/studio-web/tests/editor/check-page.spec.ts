5ccf43e7b29e0feec9714ae1444bb2ea762bac91
Delasie Torkornoo <Delasie.Torkornoo@nrc-cnrc.gc.ca>
2024-12-13 17:26:07 -0500

    test: refactored editor tests

--- packages/studio-web/tests/editor/check-page.spec.ts
+++ packages/studio-web/tests/editor/check-page.spec.ts
@@ -1,97 +1,91 @@
 import { test, expect } from "@playwright/test";
 import { testAssetsPath, disablePlausible } from "../test-commands";
 test.describe.configure({ mode: "parallel" });
-test.describe("test editor UI & UX", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/", { waitUntil: "load" });
-    disablePlausible(page);
-    await page.getByRole("button", { name: /Editor/ }).click();
-  });
-  test("should check UI", async ({ page }) => {
-    await expect(
-      page.getByRole("button", { name: "Take the tour!" }),
-      "Tour button is visible",
-    ).toBeVisible();
-    await expect(
-      page.locator("#updateRAS"),
-      "Choose file is visible",
-    ).toBeVisible();
-    let fileChooserPromise = page.waitForEvent("filechooser");
-    await page.locator("#updateRAS").click();
-    let fileChooser = await fileChooserPromise;
-    fileChooser.setFiles(testAssetsPath + "sentence-paragr.html");
-    //check audio bar
-    /**
-     * We are using css classes instead of test-id because this is an external plugin
-     * failure here means the plugin version has changed we should test impact on our app
-     */
+test("should check editor UI", async ({ page }) => {
+  await page.goto("/", { waitUntil: "load" });
+  disablePlausible(page);
+  await page.getByRole("button", { name: /Editor/ }).click();
+  await expect(
+    page.getByRole("button", { name: "Take the tour!" }),
+    "Tour button is visible",
+  ).toBeVisible();
+  await expect(
+    page.locator("#updateRAS"),
+    "Choose file is visible",
+  ).toBeVisible();
+  let fileChooserPromise = page.waitForEvent("filechooser");
+  await page.locator("#updateRAS").click();
+  let fileChooser = await fileChooserPromise;
+  fileChooser.setFiles(testAssetsPath + "sentence-paragr.html");
+  //check audio bar
+  /**
+   * We are using css classes instead of test-id because this is an external plugin
+   * failure here means the plugin version has changed we should test impact on our app
+   */
 
-    await expect(
-      page.locator("#audioToolbar"),
-      "audio bar should exist",
-    ).toHaveCount(1);
-    await expect(
-      page.locator("segment.wavesurfer-segment"),
-      "should seven audio segments",
-    ).toHaveCount(7);
-    await expect(
-      page.locator(
-        "segment.wavesurfer-segment:first-of-type > .segment-content",
-      ),
-      "audio segments text should be editable",
-    ).toHaveAttribute("contenteditable", "true");
-    await expect(
-      page.locator(
-        "segment.wavesurfer-segment:first-of-type > .wavesurfer-handle",
-      ),
-      "audio segments boundaries should exist",
-    ).toHaveCount(2);
+  await expect(
+    page.locator("#audioToolbar"),
+    "audio bar should exist",
+  ).toHaveCount(1);
+  await expect(
+    page.locator("segment.wavesurfer-segment"),
+    "should seven audio segments",
+  ).toHaveCount(7);
+  await expect(
+    page.locator("segment.wavesurfer-segment:first-of-type > .segment-content"),
+    "audio segments text should be editable",
+  ).toHaveAttribute("contenteditable", "true");
+  await expect(
+    page.locator(
+      "segment.wavesurfer-segment:first-of-type > .wavesurfer-handle",
+    ),
+    "audio segments boundaries should exist",
+  ).toHaveCount(2);
 
-    //check readalong
-    await expect(
-      page.locator("#readalongContainer"),
-      "should check that readalong is loading",
-    ).not.toBeEmpty();
-    const header = page.locator(
-      "#readalongContainer span[slot=read-along-header]",
-    );
-    await expect(header, "should have correct title").toContainText(
-      "Sentence Paragraph Page",
-    );
-    await expect(header, "should have editable title").toHaveAttribute(
-      "contenteditable",
-      "true",
-    );
-    const subheader = page.locator(
-      "#readalongContainer span[slot=read-along-subheader]",
-    );
-    await expect(subheader, "should have correct subtitle").toContainText(
-      "by me",
-    );
-    await expect(subheader, "should have editable subtitle").toHaveAttribute(
-      "contenteditable",
-      "true",
-    );
-    await page.locator("#t0b0d0p1s0").scrollIntoViewIfNeeded();
-    await expect(
-      page.locator("#t0b0d0p0s0").getByLabel("Remove translation"),
-      "should have translation for first paragraph first sentence",
-    ).toBeVisible();
-    await expect(
-      page.locator("#t0b0d0p0s1").getByRole("button", { name: "add" }),
-      "should not have translation for first paragraph second sentence",
-    ).toBeVisible();
-    await expect(
-      page.locator("#t0b0d0p1s0").getByLabel("Remove translation"),
-      "should have translation for second paragraph ",
-    ).toBeVisible();
-    await expect(
-      page.locator("#fileElem--t0b0d0"),
-      "should not have image on first page",
-    ).toHaveCount(1);
-    await expect(
-      page.locator("#t0b0d1 img"),
-      "should have image on second page",
-    ).toHaveCount(1);
-  });
+  //check readalong
+  await expect(
+    page.locator("#readalongContainer"),
+    "should check that readalong is loading",
+  ).not.toBeEmpty();
+  const header = page.locator(
+    "#readalongContainer span[slot=read-along-header]",
+  );
+  await expect(header, "should have correct title").toContainText(
+    "Sentence Paragraph Page",
+  );
+  await expect(header, "should have editable title").toHaveAttribute(
+    "contenteditable",
+    "true",
+  );
+  const subheader = page.locator(
+    "#readalongContainer span[slot=read-along-subheader]",
+  );
+  await expect(subheader, "should have correct subtitle").toContainText(
+    "by me",
+  );
+  await expect(subheader, "should have editable subtitle").toHaveAttribute(
+    "contenteditable",
+    "true",
+  );
+  await page.locator("#t0b0d0p1s0").scrollIntoViewIfNeeded();
+  await expect(
+    page.locator("#t0b0d0p0s0").getByLabel("Remove translation"),
+    "should have translation for first paragraph first sentence",
+  ).toBeVisible();
+  await expect(
+    page.locator("#t0b0d0p0s1").getByRole("button", { name: "add" }),
+    "should not have translation for first paragraph second sentence",
+  ).toBeVisible();
+  await expect(
+    page.locator("#t0b0d0p1s0").getByLabel("Remove translation"),
+    "should have translation for second paragraph ",
+  ).toBeVisible();
+  await expect(
+    page.locator("#fileElem--t0b0d0"),
+    "should not have image on first page",
+  ).toHaveCount(1);
+  await expect(
+    page.locator("#t0b0d1 img"),
+    "should have image on second page",
+  ).toHaveCount(1);
 });
--- packages/studio-web/tests/editor/edit-images.spec.ts
+++ packages/studio-web/tests/editor/edit-images.spec.ts
@@ -1,41 +1,40 @@
 import { test, expect } from "@playwright/test";
 import { testAssetsPath, editorDefaultBeforeEach } from "../test-commands";
 test.describe.configure({ mode: "parallel" });
-test.describe("test editor UI & UX", () => {
-  test("should edit images", async ({ page }) => {
-    await expect(async () => {
-      await editorDefaultBeforeEach(page);
-    }).toPass();
-    await page.locator("#updateRAS").waitFor({ state: "visible" });
-    await expect(
-      page.locator("#fileElem--t0b0d0"),
-      "should not have image on first page",
-    ).toHaveCount(1);
-    //upload a photo to page 1
-    let fileChooserPromise = page.waitForEvent("filechooser");
-    page.locator("#fileElem--t0b0d0").dispatchEvent("click");
 
-    let fileChooser = await fileChooserPromise;
-    fileChooser.setFiles(testAssetsPath + "page1.png");
-    await expect(
-      page.locator("#t0b0d0 img"),
-      "should have image on first page",
-    ).toHaveCount(1);
-    //delete photo page 2 and re-add
-    const progressBar = page.getByTestId("progress-bar");
-    const progressBarBoundingBox = await progressBar.boundingBox();
+test("should edit images (editor)", async ({ page }) => {
+  await expect(async () => {
+    await editorDefaultBeforeEach(page);
+  }).toPass();
+  await page.locator("#updateRAS").waitFor({ state: "visible" });
+  await expect(
+    page.locator("#fileElem--t0b0d0"),
+    "should not have image on first page",
+  ).toHaveCount(1);
+  //upload a photo to page 1
+  let fileChooserPromise = page.waitForEvent("filechooser");
+  page.locator("#fileElem--t0b0d0").dispatchEvent("click");
 
-    await progressBar.click({
-      force: true,
-      position: {
-        x: progressBarBoundingBox?.x || 0,
-        y: progressBarBoundingBox ? progressBarBoundingBox.width * 0.9 : 0,
-      },
-    });
-    await page.locator("#t0b0d1").getByTestId("delete-button").click();
-    await expect(
-      page.locator("#fileElem--t0b0d1"),
-      "should remove image on second page",
-    ).toHaveCount(1);
+  let fileChooser = await fileChooserPromise;
+  fileChooser.setFiles(testAssetsPath + "page1.png");
+  await expect(
+    page.locator("#t0b0d0 img"),
+    "should have image on first page",
+  ).toHaveCount(1);
+  //delete photo page 2 and re-add
+  const progressBar = page.getByTestId("progress-bar");
+  const progressBarBoundingBox = await progressBar.boundingBox();
+
+  await progressBar.click({
+    force: true,
+    position: {
+      x: progressBarBoundingBox?.x || 0,
+      y: progressBarBoundingBox ? progressBarBoundingBox.width * 0.9 : 0,
+    },
   });
+  await page.locator("#t0b0d1").getByTestId("delete-button").click();
+  await expect(
+    page.locator("#fileElem--t0b0d1"),
+    "should remove image on second page",
+  ).toHaveCount(1);
 });
