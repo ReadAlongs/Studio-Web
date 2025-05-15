8bf599c99eacf0e9aed4267335327f49542e374a
Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
2025-05-15 13:58:05 -0300

    fix: explictly set standalone=false for all components

--- packages/studio-web/src/app/text-format-dialog/text-format-dialog.component.ts
+++ packages/studio-web/src/app/text-format-dialog/text-format-dialog.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from "@angular/core";
   selector: "app-text-format-dialog",
   templateUrl: "./text-format-dialog.component.html",
   styleUrls: ["./text-format-dialog.component.sass"],
+  standalone: false,
 })
 export class TextFormatDialogComponent implements OnInit {
   constructor() {}
