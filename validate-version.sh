cb50bffcf3636a30755fd202fc682e50aeaafa89
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-01-15 12:55:38 -0500

    fix: make validate-version.sh more verbose so we know what it is doing

--- validate-version.sh
+++ validate-version.sh
@@ -10,21 +10,25 @@ fi
 VERSION=${INPUT_TAG#v}
 BAD=
 
+echo "Checking that version in packages/web-component/package.json == $VERSION"
 if [ "$(jq -r .version packages/web-component/package.json)" != "$VERSION" ]; then
   echo "ERROR: version mismatch in packages/web-component/package.json"
   BAD=1
 fi
 
+echo "Checking that version in packages/ngx-web-component/package.json == $VERSION"
 if [ "$(jq -r .version packages/ngx-web-component/package.json)" != "$VERSION" ]; then
   echo "ERROR: version mismatch in packages/ngx-web-component/package.json"
   BAD=1
 fi
 
+echo "Checking that peer dependency on @readalongs/web-component in packages/ngx-web-component/package.json == $VERSION"
 if [ "$(jq -r '.peerDependencies."@readalongs/web-component"' packages/ngx-web-component/package.json)" != "$VERSION" ]; then
   echo "ERROR: peer dependency on @readalongs/web-component version mismatch in packages/ngx-web-component/package.json"
   BAD=1
 fi
 
+echo "Checking that singleFileBundleVersion in packages/studio-web/package.json == $VERSION"
 if [ "$(jq -r .singleFileBundleVersion packages/studio-web/package.json)" != "$VERSION" ]; then
   echo "ERROR: singleFileBundleVersion mismatch in packages/studio-web/package.json"
   BAD=1
@@ -34,6 +38,7 @@ version_less_than() {
   { echo "$1"; echo "$2"; } | sort -V -C && [ "$1" != "$2" ]
 }
 
+echo Checking versions published on npmjs.com...
 NPM_VERSION_WEBC=$(npm view @readalongs/web-component version)
 echo "Published NPM @readalongs/web-component version: $NPM_VERSION_WEBC"
 NPM_VERSION_NGXWEBC=$(npm view @readalongs/ngx-web-component version)
@@ -56,4 +61,6 @@ fi
 
 if [ -n "$BAD" ]; then
   exit 1
+else
+  echo "All version checks passed."
 fi
